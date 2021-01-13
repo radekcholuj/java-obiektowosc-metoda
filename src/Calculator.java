b2513d1a9805aeee065a80d32a4e6c462ff1e396
Radoslaw Choluj <radekcholuj@wp.pl>
2021-01-13 21:23:29 +0100

    #8 średnia i suma elemtów tablicy

--- src/Calculator.java
+++ src/Calculator.java
@@ -5,4 +5,15 @@ public class Calculator {
     int sum5Arguments(int a , int b, int c, int d, int e){
         return a+b+c+d+e;
     }
+
+    int sumArray(int[] values){
+        int sum = 0;
+        for (int v : values){
+            sum+=v;
+        }
+        return sum;
+    }
+    double avg(int[] values){
+        return sumArray(values)/ values.length;
+    }
 }
